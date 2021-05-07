7c7cae372e02beabbc20d28c1027351d654a7e6e
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-05-07 19:25:10 +0530

    Day 2 files

---
+++ 02_02_main.py
@@ -0,0 +1,22 @@
+# Day 2
+
+print("Hello World")
+print("Welcome to Day 2")
+
+## let us input your favorite number
+name = input("Enter your name:")
+
+# num = int(input("Enter your fav num:"))
+# print('Hello', name, '! Your fav num is', num)
+
+print(type(name))
+
+sal = input("Enter salary:")
+# eval can evaluate expression
+# whether we give int or str it will work
+sal = eval(sal)
+print(type(sal))
+
+
+
+
