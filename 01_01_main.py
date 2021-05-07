729e9523af822a18c8560b6187251957f126fcc3
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-05-07 19:24:09 +0530

    Day 1 files

---
+++ 01_01_main.py
@@ -0,0 +1,18 @@
+
+# Getting started with python
+
+# Let us get our hand dirty
+
+
+
+## This is how we print something
+print("Hello World")
+print("Welcome to Inspire Python Course")
+print("This is Day-1")
+
+
+print()
+
+## This is we take input as string
+name = input("Enter your name:")
+print("Hello", name, "! Welcome to the session.")
