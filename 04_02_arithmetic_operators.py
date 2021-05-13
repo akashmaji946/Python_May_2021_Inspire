715ddb576be57f6bcf203cbd9440c8e35677980d
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-05-13 20:37:24 +0530

    Day 4 files

---
+++ 04_02_arithmetic_operators.py
@@ -0,0 +1,31 @@
+x = 15
+y = 4
+
+# Output: x + y = 19
+print('x + y =', x+y)
+
+# Output: x - y = 11
+print('x - y =', x-y)
+
+# Output: x * y = 60
+print('x * y =', x*y)
+
+# Output: x / y = 3.75
+print('x / y =', x/y)
+
+# Output: x // y = 3
+print('x // y =', x//y)
+
+# Output: x ** y = 50625
+print('x ** y =', x**y)
+
+
+# + - * /    //     %    **
+
+x = 6
+y = 3
+
+print(2**3)
+print(2 * 3)
+print(x / y)   # float
+print(x // y)  # int
