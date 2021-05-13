cf5f189a00c1f6765e42c459de7f2e7fafb24b10
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-05-13 20:44:11 +0530

    Day 7 files

---
+++ 07_01_main.py
@@ -0,0 +1,30 @@
+
+# draw square in Python Turtle
+import turtle
+
+# turtle object
+t = turtle.Turtle()
+
+# distance
+s = 100
+
+# # drawing first side
+# t.forward(s) # Forward turtle by s units
+# t.left(90) # Turn turtle by 90 degree
+
+# # drawing second side
+# t.forward(s) # Forward turtle by s units
+# t.left(90) # Turn turtle by 90 degree
+
+# # drawing third side
+# t.forward(s) # Forward turtle by s units
+# t.left(90) # Turn turtle by 90 degree
+
+# # drawing fourth side
+# t.forward(s) # Forward turtle by s units
+# t.left(90) # Turn turtle by 90 degree
+
+for i in range(4):
+	t.forward(100)
+	t.left(90)
+	
---
+++ 07_02_even_odd.py
@@ -0,0 +1,14 @@
+n = 5
+# '%' is a remainder operator
+if n % 2 == 1:
+	print("n is Even")
+else:
+	print("n is Odd")
+
+#optimised
+m = 9
+# '&' is a bitwise operators
+if m & 1:
+	print("m is Odd")
+else:
+	print("m is Even")
