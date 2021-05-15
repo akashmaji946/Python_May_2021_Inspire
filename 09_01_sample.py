4cf2d4ad6995a18f6e359f18ddfdc29e6694ed53
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-05-15 20:23:34 +0530

    Day 9 files

---
+++ 09_01_sample.py
@@ -0,0 +1,20 @@
+# 1st => declaration
+# def  => define
+# use: to define a new func
+def say_hello():
+	print("hello")
+	print("hi")
+	print("bye")
+
+# func. call (name +  () )
+say_hello()
+# #......\
+# #......
+# #......
+say_hello()
+#.....
+
+
+
+# say_hello()
+
