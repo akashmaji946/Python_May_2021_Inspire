391bd93ca24cabb728ccbc986d7b8c3616a40608
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-05-17 20:20:41 +0530

    Day 6 files

---
+++ 06_01_if_else.py
@@ -0,0 +1,41 @@
+
+# if else
+############################
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+#####################################
+
+if 6 == 6:
+	print("6 is equal to itself")
+
+
+if 5 < 6:
+	print("5 is less than 6")
+
+# single line
+a = 7
+b = 6
+if a > b: print("a is greater than b")
+
+
+
+
+# else necesaary has to be paired with if
+
+kauravs = 100
+pandavas = 5
+if kauravs == pandavas:
+	print("they are equally powerful")
+else:
+	print("pandavas are powerful")
+
+
+# simple way (like ternary opr.)
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
---
+++ 06_03_for_loop.py
@@ -0,0 +1,21 @@
+#for loop in python is special foreach loop
+n = 4
+for i in range(0, n):
+    print(i)
+
+
+list = ["inspire", "python", "course"]
+for index in range(len(list)):
+    print(list[index])
+
+
+#print table of 2
+
+for i in range(2, 21, 2):
+	print(i)
+
+# for loop can iterate over strings
+
+string = "IPC-2021"
+for ch in string:
+	print(ch, end = " ")
