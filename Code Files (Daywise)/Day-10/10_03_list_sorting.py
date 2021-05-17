1595c7bd6da6986cf65b631a022b9a59ead75959
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-05-17 19:01:29 +0530

    Day 10 code files

---
+++ Code Files (Daywise)/Day-10/10_03_list_sorting.py
@@ -0,0 +1,11 @@
+l = [9, 3, 100, -1]
+
+# l.sort() => changes the list itself
+# l.sort()
+
+# sorted() => new list deta hai and does not change original list
+l2 = sorted(l, reverse = True)
+print(l)
+print(l2)
+
+
