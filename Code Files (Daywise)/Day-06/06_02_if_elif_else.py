b8994a8cdd0e3a733438248cde49d82ede1b5ee1
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-05-17 20:21:20 +0530

    Day 6  code files

---
+++ Code Files (Daywise)/Day-06/06_02_if_elif_else.py
@@ -0,0 +1,22 @@
+
+
+# elif gives us more options
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+
+#nested if
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
---
+++ Code Files (Daywise)/Day-06/06_04_while_loop.py
@@ -0,0 +1,22 @@
+#while -> execute as long is condition is true
+count = 0
+while (count < 3):   
+    count = count + 1
+    print("Hello PyBuddies")
+
+#print all odd numbers between 0 and 10
+
+i = 0
+while i<=10:
+	if i%2==1:
+		print(i)
+	i += 1
+
+
+#infinite loop: dont run this
+count = 0
+while (count == 0): 
+	print("Hello")
+
+
+
