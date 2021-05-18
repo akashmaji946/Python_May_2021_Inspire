c0d0adf2bc9e2da1c176a7473dd4cc779a4918cb
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-05-18 18:50:14 +0530

    Day 11 code files

---
+++ Code Files (Daywise)/Day-11/11_03_slicing.py
@@ -0,0 +1,10 @@
+
+
+string = "india"
+        # 01234
+print(string[0:2]) 
+
+#reverse indexing
+print(string[-2])
+
+# 0,n  => 0,....n-1
---
+++ Code Files (Daywise)/Day-11/11_04_main.py
@@ -0,0 +1,33 @@
+
+# Welcome to day 11
+
+# Demonstration of hash function
+
+# Hash function -> hash value
+
+#########################################
+
+# hash for integer unchanged
+print('Hash for 181 is:', hash(181))
+
+# hash for decimal
+print('Hash for 181.23 is:',hash(181.23))
+
+# hash for string
+print('Hash for Python is:', hash('Python'))
+
+###########################################
+
+# tuple of vowels
+vowels = ('a', 'e', 'i', 'o', 'u')
+
+print('The hash is:', hash(vowels))
+
+############################################
+
+# tuple of vowels
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+print('The hash of list is:', hash(vowels))
+
+##########################################
