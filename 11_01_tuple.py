a7e7bf5a485c142836ed7f6d5ef70619fa3436e0
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-05-18 18:48:53 +0530

    Day 11 files

---
+++ 11_01_tuple.py
@@ -0,0 +1,13 @@
+
+#tuple->
+
+tup = (1, 2, 1,  3, 5, 1)
+print(tup)
+
+print(max(tup))
+
+# tup[0] = -1
+# print(tup)
+
+print(tup.count(1))
+
---
+++ 11_02_traverse.py
@@ -0,0 +1,22 @@
+
+tup = ("1", "2", 3, 4.6)
+
+# for item in tup:
+# 	print(item)
+
+
+# break and continue
+
+i = 0
+
+# for i in range(0, 11):
+# 	if i % 2 == 0:
+# 		continue
+# 	print(i)
+
+for i in range(0, 11):
+	if i == 5:
+		break
+	print(i)
+
+
