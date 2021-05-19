2c7d5aa2e5cce43aa4a790763806e82eee515548
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-05-19 19:04:41 +0530

    Day 12 code files

---
+++ Code Files (Daywise)/Day-12/12_02_sets.py
@@ -0,0 +1,41 @@
+
+# unordered
+myset = {1, 12, 3, 5, 8, 9}
+print(myset)
+
+print(len(myset))
+
+myset.add(12)
+
+print(myset)
+print(len(myset))
+
+
+print(7 in myset)
+
+
+set1 = {"abc", 123, True}
+set2 = {"def", 456, False}
+
+set1.update(set2)
+
+print(set1)
+print(set2)
+
+ages = {18, 19, 20, 21, 20, 19, 18}
+# 18   19   20    21
+print(len(ages))
+
+#set => count unique no. of items
+
+set3 = {1, 2, 3}
+print(set3)
+set3.remove(3)
+print(set3)
+
+
+
+
+
+
+
