453a938fc9246a66071769086caf4ac557be6ccc
AKASH MAJI <49202839+akashmaji946@users.noreply.github.com>
2021-05-27 21:12:57 +0530

    Day 15 code files

---
+++ Code Files (Daywise)/Day-15/15_01_empty_class.py
@@ -0,0 +1,25 @@
+
+# empty class
+
+class Pen:
+    # yha pe kuch nhi hoga
+    # empty class
+    pass
+
+mypen = Pen()
+print(mypen)
+
+# id => 0x7f019e420210 (memory adress)
+
+print(id(mypen))  # id() => identity => get mme the address
+
+# baad me dekh lenge
+print(139644926736464 == 0x7f019e420210)
+
+
+
+
+
+
+    
+
